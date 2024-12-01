c19d7090a840f44590199112e30d6b534bbe5eab
pavlovy <pavlovy@wix.com>
2024-12-01 12:04:12 +0200

    day 1

--- src/main/scala/advent2024/DataReader.scala
+++ src/main/scala/advent2024/DataReader.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 object DataReader {
 
   def getInput(file: String): String = {
-    val source = Source.fromFile(s"src/main/resources/data/day1/$file" )
+    val source = Source.fromFile(s"src/main/resources/data/$file" )
     try source.getLines().mkString("\n") finally source.close()
   }
 
--- src/main/scala/advent2024/day1/Day1.scala
+++ src/main/scala/advent2024/day1/Day1.scala
@@ -1,5 +1,30 @@
 package advent2024.day1
 
+import advent2024.DataReader
+
 object Day1 extends App {
-  println("test")
+  println(partOne())
+  println(partTwo())
+
+  def partOne(): Int = {
+    val (nums1, nums2) = getFormattedInput
+    nums1.sorted.zip(nums2.sorted)
+      .map(t => Math.abs(t._1 - t._2))
+      .sum
+  }
+
+  def partTwo(): Int = {
+    val (nums1, nums2) = getFormattedInput
+    val multiplier = nums2.groupBy(identity).view.mapValues(_.size)
+    nums1.map(num => multiplier.getOrElse(num, 0) * num).sum
+  }
+
+  def getFormattedInput: (Seq[Int], Seq[Int]) = {
+    DataReader.getInput("data1.txt")
+      .split("\n")
+      .map(s => {
+        val row = s.split(" +").map(_.toInt)
+        (row(0), row(1))
+      }).toSeq.unzip
+    }
 }
