a11a2f0462bee6704cf85489ac93444f4d0ecc45
pavlovy <pavlovy@wix.com>
2024-12-03 11:20:44 +0200

    day 3p2

--- src/main/scala/advent2024/solutions/Day3.scala
+++ src/main/scala/advent2024/solutions/Day3.scala
@@ -4,6 +4,7 @@ import advent2024.DataReader
 
 object Day3 extends App {
   println(Part1.getSolution)
+  println(Part2.getSolution)
 }
 
 object Part1 {
@@ -18,6 +19,24 @@ object Part1 {
       .map(s => (s.group(1).toInt, s.group(2).toInt)).toSeq)
       .map(t => t._1 * t._2)
       .sum
+  }
+}
 
+object Part2 {
+  val input = DataReader.getInput("data1.txt")
+  def getSolution: Long = {
+    def go(s: String, acc: Long, isDo: Boolean): Long = {
+      s match {
+        case "" => acc
+        case s"do()$rest" => go(rest, acc, true)
+        case s"don't()$rest" => go(rest, acc, false)
+        case s"mul(${num1},${num2})$rest"  if num1.forall(_.isDigit) &&
+          num2.forall(_.isDigit) &&
+          num1.length <= 3 && num2.length <= 3 &&
+          num1.length >= 0 && num2.length >= 0 && isDo => go(rest, (num1.toLong * num2.toLong) + acc, isDo)
+        case _ => go(s.tail, acc, isDo)
+      }
+    }
+    go(input, 0, true)
   }
 }
